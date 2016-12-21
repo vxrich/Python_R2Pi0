3b19fce25bb860573d45cf13a541cb037bbfcb50
Tosa95 <davide.tosatto95@gmail.com>
2016-12-21 16:19:44 +0000

    Aggiunta libreria movimento robottino

---
+++ motor_control.py
@@ -0,0 +1,267 @@
+#
+#	Autori: Tosatto Davide, Riccardo Grespan
+#
+#	Modulo di controllo motori.
+#	Lo scopo e' quello di poter muovere il robottino semplicemente dando
+#	una velocita' e una rotazione.
+#
+ 
+
+
+import RPi.GPIO as GPIO 
+import time
+
+#Modalita' di spegnimento motore:
+# MODE_SC: Short Circuit: pone il motore a massa da entrambi i pin in modo da farlo fremare prima
+# MODE_FL: Floating: lascia il motore floatante: si ferma dopo
+MODE_SC = 0
+MODE_FL = 1
+
+HIGH = 1
+LOW = 0
+
+DEFAULT_DUTY = 50
+DEFAULT_FREQ = 10
+
+def limit (value, min, max):
+	if value < min:
+		return min
+	elif value > max:
+		return max
+	return value
+
+class Motor:
+	
+	_STOPPED = 0
+	_FORWARD = 1
+	_BACKWARD = 2
+	
+	_forward_pin = None
+	_backward_pin = None
+	_pwm_forward = None
+	_pwm_backward = None
+	
+	def __init__ (self, forward_pin, backward_pin):
+		self._forward_pin = forward_pin
+		self._backward_pin = backward_pin
+		self._state = self._STOPPED
+		self._speed = 0
+		
+		
+	def initialize (self, pwm_freq=DEFAULT_FREQ):
+	
+		print "%d %d" % (self._forward_pin, self._backward_pin)
+	
+		GPIO.setup(self._forward_pin, GPIO.OUT)
+		GPIO.setup(self._backward_pin, GPIO.OUT)
+		self._pwm_forward = GPIO.PWM(self._forward_pin, pwm_freq)
+		self._pwm_backward = GPIO.PWM(self._backward_pin, pwm_freq)
+		self._pwm_backward.start(0)
+		self._pwm_forward.start(0)
+		self.move(0)
+		
+	def _setHighImpedance ():
+	
+		GPIO.setup(self._forward_pin, GPIO.IN)
+		GPIO.setup(self._backward_pin, GPIO.IN)
+		
+	def _setLowImpedance ():
+	
+		GPIO.setup(self._forward_pin, GPIO.OUT)
+		GPIO.setup(self._backward_pin, GPIO.OUT)
+		
+	def _stopPwm (self, pwm):
+		pwm.stop()
+		time.sleep(0.01)
+		
+	def _setPwm (self, pwm, duty, state):
+		if self._state!=state:
+			#self.stop()
+			#time.sleep(0.5)
+			self.stop(MODE_FL)
+			pwm.stop()
+			pwm.start(duty)
+		else:
+			pwm.ChangeDutyCycle(duty)
+		
+	def forward (self, duty=DEFAULT_DUTY):
+#		print "Forward %d" % duty
+		self._pwm_backward.ChangeDutyCycle(0)
+		
+		self._pwm_forward.ChangeDutyCycle(duty)
+		
+		self._state = self._FORWARD
+		
+	def backward (self, duty=DEFAULT_DUTY):
+#		print "Backward %d" % duty
+		self._pwm_forward.ChangeDutyCycle(0)
+		
+		self._pwm_backward.ChangeDutyCycle(duty)
+		
+		self._state = self._BACKWARD
+		
+	def move (self, speed):
+	
+		print "Move %d" % speed
+		
+	
+		if speed > 0:
+			speed = limit(speed, 0, 100)
+			self.forward(speed)
+		elif speed < 0:
+			speed = limit(-speed, 0, 100)
+			self.backward(speed)
+		else:
+			self.stop(MODE_FL)
+			
+		self._speed = speed
+		
+	def move_delta (self, speed_delta):
+		new_speed = self.getSpeed() + speed_delta
+		self.move(new_speed)
+		
+	def stop (self, mode=MODE_SC):
+	
+#		print "Stopped"
+
+		
+	
+		if mode==MODE_SC:
+			self._pwm_forward.ChangeDutyCycle(100)
+			self._pwm_backward.ChangeDutyCycle(100)
+			
+		else:
+			self._pwm_forward.ChangeDutyCycle(0)
+			self._pwm_backward.ChangeDutyCycle(0)
+			
+		#time.sleep(3/DEFAULT_FREQ)
+
+			
+		self._state = self._STOPPED
+		
+	def getState (self):
+		return self._state
+		
+	def getSpeed (self):
+		return delf._speed
+
+DX_FW = 23
+DX_BW = 24
+
+SX_FW = 10
+SX_BW = 11
+		
+class MovementController:
+	
+	_MOTOR_DX = None
+	_MOTOR_SX = None
+	_speed = 0
+	_rotation = 0
+	
+	def __init__ (self):
+		self._MOTOR_DX = Motor(DX_FW, DX_BW)
+		self._MOTOR_SX = Motor(SX_FW, SX_BW)
+		
+	def initialize(self):
+		self._MOTOR_DX.initialize()
+		self._MOTOR_SX.initialize()
+		self.stop()
+		
+	def stop (self):
+		self._MOTOR_DX.move(0)
+		self._MOTOR_SX.move(0)
+		self._speed = 0
+		self._rotation = 0
+		
+	def applyMovement (self, speed, rotation):
+		"""
+			Applica un movimento al robottino.
+			
+			Usa come velocita' base speed e somma/sottrae rotation da
+			una parte e dall'altra in base alla direzione di rotazione
+			
+			Input:
+				speed: velocita' di movimento (>0 -> avanti | <0 -> indietro)
+				rotation: velocita' di rotazione (>0 -> orario | <0 -> antiorario)
+		"""
+		
+		dx_speed = speed
+		sx_speed = speed
+		
+		#Orario
+		if rotation > 0:
+		
+			rotation_abs = limit(rotation, 0, 100)
+			
+			#Meno spinta a dx
+			dx_speed = speed - rotation_abs
+			#Piu' spinta a sx
+			sx_speed = speed + rotation_abs
+		
+		#Antiorario
+		elif rotation < 0:
+			
+			rotation_abs = limit(-rotation, 0, 100)
+			
+			#Piu' spinta a dx
+			dx_speed = speed + rotation_abs
+			#Meno spinta a sx
+			sx_speed = speed - rotation_abs
+			
+		print "applyMovement SX:%d DX:%d" % (sx_speed, dx_speed)
+			
+		self._MOTOR_DX.move(dx_speed)
+		self._MOTOR_SX.move(sx_speed)
+		
+		self._speed = speed
+		self._rotation = rotation
+		
+	def rotate (self, rotation):
+	
+		self.applyMovement (self._speed, rotation)
+		
+	def move (self, speed):
+		
+		self.applyMovement (speed, self._rotation)
+		
+
+		
+if __name__ == "__main__":
+	#TEST
+	
+	
+	#Test Motor
+	GPIO.setmode(GPIO.BCM)
+	motor = Motor(23,24)
+	motor.initialize()
+	
+	ctrl = MovementController()
+	ctrl.initialize()
+	
+	#GPIO.setup(23, GPIO.OUT)
+	#GPIO.output(23, 1)
+	
+	#time.sleep(100)
+	
+	#ctrl.rotate(10)
+	
+	for i in reversed(range(-100, 0, 5)):
+			ctrl.move(i)
+			time.sleep(0.1)
+	
+	for i in range(10):
+	
+		for i in range(-100, 0, 5):
+			ctrl.move(i)
+			time.sleep(0.1)
+			
+			
+		for i in range(0, 100, 5):
+			ctrl.move(i)
+			time.sleep(0.1)
+			
+		for i in reversed(range(-100, 100, 5)):
+			ctrl.move(i)
+			time.sleep(0.1)	
+		
+	GPIO.cleanup()
