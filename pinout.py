2910344832672d19953e8f5cd55e0c96db94fabc
vxrich <riccardo.grespan6@gmail.com>
2017-01-08 23:12:46 +0100

    Aggiunti pin a pinout.py

--- pinout.py
+++ pinout.py
@@ -1,4 +1,4 @@
-import RPi.GPIO as GPIO 
+import RPi.GPIO as GPIO
 
 MOTOR_SX_FW = 19
 MOTOR_SX_BW = 26
@@ -7,3 +7,6 @@ MOTOR_DX_FW = 24
 MOTOR_DX_BW = 23
 
 BUZZER = 13
+
+SENSOR_TRIG = 24
+SENSOR_ECHO = 23
